cb8f66f4f272e35ab421c00e369b5d6c60b27398
HemishPansuriya <hemishnandlal@gmail.com>
2025-08-11 17:50:34 +0530

    first

--- interview/settings.py
+++ interview/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['SECRET']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['20.192.170.8', '169.254.131.2', '169.254.131.3', 'jivihire-b0bvbvh9aecpghbn.centralindia-01.azurewebsites.net', 'jivihire.com','recruitsmart.co', '127.0.0.1','localhost']
 
